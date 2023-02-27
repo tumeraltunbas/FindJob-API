a9b1cf6a4aef16a785e243e1656a49f8bf43d14a
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-27 18:05:01 +0300

    Add errorHandler middleware

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
 import { connectDb } from "./helpers/database/dbConnection.js";
-
+import { errorHandler } from "./middlewares/error/errorHandler.js";
 
 config({path:"./config/config.env"});
 const app = express();
 
 connectDb();
 
-
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));
