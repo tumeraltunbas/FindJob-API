c02e80b919f54ccbc1b2b71152b08ff2d8c7e5d9
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-02-27 18:12:03 +0300

    Add routing system

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import { connectDb } from "./helpers/database/dbConnection.js";
 import { errorHandler } from "./middlewares/error/errorHandler.js";
+import routes from "./routes/index.js";
 
 config({path:"./config/config.env"});
 const app = express();
 
 connectDb();
-
+app.use("/api",routes);
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log(`Server started at ${process.env.PORT}`));
