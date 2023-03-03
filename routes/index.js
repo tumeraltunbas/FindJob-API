e1af7452de450a726b81334a8336f37ae344e4b1
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-03 13:26:12 +0300

    Add certificate routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ import authRoutes from "./auth.js";
 import userRoutes from "./user.js";
 import experienceRoutes from "./experience.js";
 import educationRoutes from "./education.js";
+import certificateRoutes from "./certificate.js";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/experience", experienceRoutes);
 router.use("/education", educationRoutes);
+router.use("/certificate", certificateRoutes);
 
 export default router;
