8ce6ebc5ccd7b1ab10841817adc0b61835769294
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-03 15:05:53 +0300

    Add company routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import userRoutes from "./user.js";
 import experienceRoutes from "./experience.js";
 import educationRoutes from "./education.js";
 import certificateRoutes from "./certificate.js";
+import companyRoutes from "./company.js";
 
 const router = Router();
 router.use("/auth", authRoutes);
@@ -11,5 +12,6 @@ router.use("/user", userRoutes);
 router.use("/experience", experienceRoutes);
 router.use("/education", educationRoutes);
 router.use("/certificate", certificateRoutes);
+router.use("/company", companyRoutes);
 
 export default router;
