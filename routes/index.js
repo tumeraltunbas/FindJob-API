9fae1dfdbde695620d8160c201ed79939ea7b825
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-03 12:13:54 +0300

    Add experience routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import authRoutes from "./auth.js";
 import userRoutes from "./user.js";
+import experienceRoutes from "./experience.js";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/experience", experienceRoutes);
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,10 @@
 import {Router} from "express";
-import { updatePersonalInformations, addWorkExperience, deleteWorkExperience, updateWorkExperience } from "../controllers/user.js";
+import { updatePersonalInformations } from "../controllers/user.js";
 import {getAccessToRoute} from "../middlewares/auth/auth.js";
-import { isWorkExperienceExists } from "../middlewares/query/queryMiddleware.js";
 
 
 const router = Router();
 router.put("/personal-informations", getAccessToRoute, updatePersonalInformations);
-router.post("/work-experiences", getAccessToRoute, addWorkExperience);
-router.delete("/work-experiences/:workExperienceId", [getAccessToRoute, isWorkExperienceExists], deleteWorkExperience);
-router.put("/work-experiences/:workExperienceId", [getAccessToRoute, isWorkExperienceExists], updateWorkExperience);
+
 
 export default router;
