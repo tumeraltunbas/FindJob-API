22f02531a26de2119c89a3586879d0e52d6b9fe5
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-09 15:09:01 +0300

    Revert "Update file refactor"
    This reverts commit e58a7bb8afa6eb19462caeb963e30329066b777d.

--- controllers/job.js
+++ controllers/job.js
@@ -78,6 +78,60 @@ export const createJob = async(req, res, next) => {
     }
 }
 
+export const applyJob = async(req, res, next) => {
+    try{
+        const {jobId} = req.params;
+
+        const job = await Job.findOne({
+            _id:jobId,
+            isVisible:true,
+        }).select("_id appliedUsers appliedUserCount");
+
+        if(job.appliedUsers.includes(req.user.id)){
+            return next(new CustomError(400, "You have already applied this job"));
+        }
+
+        job.appliedUsers.push(req.user.id);
+        job.appliedUserCount += 1;
+        await job.save();
+
+        return res
+        .status(200)
+        .json({success:true, message:"You have successfully applied this job"});
+
+    }
+    catch(err){
+        return next(err);
+    }
+}
+
+export const unApplyJob = async(req, res, next) => {
+    try{
+        const {jobId} = req.params
+
+        const job = await Job.findOne({
+            _id:jobId,
+            isVisible:true
+        })
+        .select("_id appliedUsers appliedUserCount");
+
+        if(!job.appliedUsers.includes(req.user.id)){
+            return next(new CustomError(400, "You have not already applied this job"));
+        }
+
+        job.appliedUsers.splice(req.user.id, 1);
+        job.appliedUserCount -= 1;
+        await job.save();
+
+        return res
+        .status(200)
+        .json({success:true, message:"You have successfully unapplied this job"});
+
+    }
+    catch(err){
+        return next(err);
+    }
+}
 
 export const getAppliedUsers = async(req, res, next) => {
     try{
