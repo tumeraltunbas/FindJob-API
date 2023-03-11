7a6896658f198845db7176596ce09ecc1fab1d5a
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-11 12:13:56 +0300

    Add serving static files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { errorHandler } from "./middlewares/error/errorHandler.js";
 import routes from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import fileUpload from "express-fileupload";
+import RootPath from "app-root-path";
 
 config({path:"./config/config.env"});
 const app = express();
@@ -12,6 +14,8 @@ const app = express();
 connectDb();
 app.use(cookieParser());
 app.use(express.json());
+app.use(fileUpload({fileSize: 5 * 1024 * 1024}));
+app.use(express.static(RootPath.path + "\\public"));
 app.use(cors({origin:true, credentials:true}));
 app.use("/api",routes);
 app.use(errorHandler);
